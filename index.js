97892a9b76c88a1d11770054becec6921552a40a
lameck <lameck2@hotmail.com>
2018-12-23 23:31:14 -0300

    The script worksgit add .

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const request = require("request");
 
-const url = "https://alpha.wallhaven.cc/wallpaper/79283";
+const url = "https://alpha.wallhaven.cc/wallpaper/396523";
 
 request(url, (err, res, html) => {
   if (!err && res.statusCode == 200) {
     const $ = cheerio.load(html);
 
-    const scrollBox = $(".scrollbox");
+    const src = $(".scrollbox")
+      .children()
+      .attr("data-cfsrc")
+      .substring(2);
+    const wallpaperId = $(".scrollbox")
+      .children()
+      .attr("data-wallpaper-id");
 
-    
+    console.log(src);
+    request
+      .get(`https://${src}`)
+      .on("error", err => console.log(err))
+      .pipe(fs.createWriteStream(`wallpaper-${wallpaperId}.png`));
   }
+  console.log("Download done");
 });
