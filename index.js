fcbf38b7d5e40cdea688d021556bea9499551957
lamecksilva <lameck2@hotmail.com>
2020-05-11 17:34:53 -0300

    FEAT: Set Wallpaper, and delete the old one

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 const request = require('request');
+const { exec } = require('child_process');
 
 // getUrl = () => {
 // 	var argv = require('minimist')(process.argv.slice(2));
@@ -12,6 +13,44 @@ const request = require('request');
 
 // const url = getUrl();
 
+function setWallPaper(wallpaperFileName) {
+	exec(
+		`gsettings set org.cinnamon.desktop.background picture-uri "file:///home/lameco/Documents/WallhavenDownload/${wallpaperFileName}"`,
+		(err, stdout, stderr) => {
+			if (err) {
+				console.error(err);
+				process.exit(1);
+			}
+
+			if (stderr) {
+				console.log(stderr);
+				process.exit(1);
+			}
+			// console.log(stdout);
+		}
+	);
+}
+
+function removeOldFile() {
+	fs.readdir('downloads', (err, files) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+
+		const fileName = files[0];
+
+		files.length &&
+			fs.unlink(`downloads/${fileName}`, (err) => {
+				if (err) {
+					console.error('Error while removing old file');
+					process.exit(1);
+				}
+				console.log('Old File Deleted');
+			});
+	});
+}
+
 function downloadWallpaper(url) {
 	request(url, (err, res, html) => {
 		if (!err && res.statusCode == 200) {
@@ -21,14 +60,17 @@ function downloadWallpaper(url) {
 
 			const wallpaperId = $('.scrollbox').children().attr('data-wallpaper-id');
 
+			const wallpaperFileName = `downloads/${new Date().getTime()}-${wallpaperId}.png`;
+
 			if (!fs.existsSync('downloads')) fs.mkdirSync('downloads');
 
 			request
 				.get(src)
-				.on('error', (err) => console.log(err))
-				.pipe(fs.createWriteStream(`downloads/wallpaper-${wallpaperId}.png`))
+				.on('request', () => console.log('Starting Download'))
+				.pipe(fs.createWriteStream(wallpaperFileName))
 				.on('close', () => {
 					console.log('Download done');
+					setWallPaper(wallpaperFileName);
 				})
 				.on('error', () => console.error('Download Error'));
 		}
@@ -42,15 +84,44 @@ request('https://wallhaven.cc/random', (err, res, html) => {
 	if (!err && res.statusCode === 200) {
 		const $ = cheerio.load(html);
 
-		// const src = $('.thumb').children().attr('.preview');
 		const src = $('.thumb')?.toArray()[0]?.children[1]?.attribs?.href;
 
 		console.log(src);
 
 		if (src) {
 			downloadWallpaper(src);
+			removeOldFile();
 		} else {
 			console.error('WallPaper Random URL not found');
 		}
 	}
 });
+
+// fs.readdir('downloads', (err, files) => {
+// 	if (err) console.error(err);
+
+// 	console.log(files);
+
+// 	console.log(new Date());
+// 	console.log(new Date().getTime());
+// 	const newFileName = `${new Date().getTime()}-${files[0]}`;
+
+// 	console.log(newFileName);
+// 	console.log(newFileName.split('-')[0]);
+// 	const newFileNameDate = new Date(parseInt(newFileName.split('-')[0]));
+
+// 	console.log(newFileNameDate);
+
+// 	const oldDate = new Date(1589226980713);
+// 	console.log(oldDate);
+// 	console.log(newFileNameDate.getTime() > oldDate.getTime());
+// 	// 1589226980713
+
+// 	fs.unlink('downloads/wallpaper-4lplv2.png', (err) => {
+// 		if (err) {
+// 			console.error('Error while removing old file');
+// 			process.exit(1);
+// 		}
+// 		console.log('Old File Deleted');
+// 	});
+// });
