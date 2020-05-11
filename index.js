3e32c1924e78ed89924ca63d7e241075809317f6
lamecksilva <lameck2@hotmail.com>
2020-05-11 13:28:31 -0300

    FIX: Version alpha to current

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const cheerio = require("cheerio");
-const fs = require("fs");
-const request = require("request");
+const cheerio = require('cheerio');
+const fs = require('fs');
+const request = require('request');
 
 getUrl = () => {
-  var argv = require("minimist")(process.argv.slice(2));
-  if (argv.u !== null && argv.u !== undefined) {
-    console.dir(argv.u);
-    return argv.u;
-  }
+	var argv = require('minimist')(process.argv.slice(2));
+	if (argv.u !== null && argv.u !== undefined) {
+		console.dir(argv.u);
+		return argv.u;
+	}
 };
 
 const url = getUrl();
 
 request(url, (err, res, html) => {
-  if (!err && res.statusCode == 200) {
-    const $ = cheerio.load(html);
+	if (!err && res.statusCode == 200) {
+		const $ = cheerio.load(html);
 
-    const src = $(".scrollbox")
-      .children()
-      .attr("data-cfsrc")
-      .substring(2);
-    const wallpaperId = $(".scrollbox")
-      .children()
-      .attr("data-wallpaper-id");
+		const src = $('.scrollbox').children().attr('data-cfsrc');
 
-    console.log(src);
-    request
-      .get(`https://${src}`)
-      .on("error", err => console.log(err))
-      .pipe(fs.createWriteStream(`wallpaper-${wallpaperId}.png`));
-  }
-  console.log("Download done");
+		const wallpaperId = $('.scrollbox').children().attr('data-wallpaper-id');
+
+		console.log(src);
+
+		request
+			.get(src)
+			.on('error', (err) => console.log(err))
+			.pipe(fs.createWriteStream(`wallpaper-${wallpaperId}.png`))
+			.on('close', () => {
+				console.log('Download done');
+			});
+	}
 });
