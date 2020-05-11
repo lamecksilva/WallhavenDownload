10d9bcfcb3392f4d20ea4718ab1a829124f18719
lamecksilva <lameck2@hotmail.com>
2020-05-11 13:57:45 -0300

    FEAT: Random download

--- index.js
+++ index.js
@@ -2,32 +2,50 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const request = require('request');
 
-getUrl = () => {
-	var argv = require('minimist')(process.argv.slice(2));
-	if (argv.u !== null && argv.u !== undefined) {
-		console.dir(argv.u);
-		return argv.u;
-	}
-};
+// getUrl = () => {
+// 	var argv = require('minimist')(process.argv.slice(2));
+// 	if (argv.u !== null && argv.u !== undefined) {
+// 		console.dir(argv.u);
+// 		return argv.u;
+// 	}
+// };
 
-const url = getUrl();
+// const url = getUrl();
 
-request(url, (err, res, html) => {
-	if (!err && res.statusCode == 200) {
+function downloadWallpaper(url) {
+	request(url, (err, res, html) => {
+		if (!err && res.statusCode == 200) {
+			const $ = cheerio.load(html);
+
+			const src = $('.scrollbox').children().attr('data-cfsrc');
+
+			const wallpaperId = $('.scrollbox').children().attr('data-wallpaper-id');
+
+			request
+				.get(src)
+				.on('error', (err) => console.log(err))
+				.pipe(fs.createWriteStream(`wallpaper-${wallpaperId}.png`))
+				.on('close', () => {
+					console.log('Download done');
+				})
+				.on('error', () => console.error('Download Error'));
+		}
+	});
+}
+
+request('https://wallhaven.cc/random', (err, res, html) => {
+	if (!err && res.statusCode === 200) {
 		const $ = cheerio.load(html);
 
-		const src = $('.scrollbox').children().attr('data-cfsrc');
-
-		const wallpaperId = $('.scrollbox').children().attr('data-wallpaper-id');
+		// const src = $('.thumb').children().attr('.preview');
+		const src = $('.thumb')?.toArray()[0]?.children[1]?.attribs?.href;
 
 		console.log(src);
 
-		request
-			.get(src)
-			.on('error', (err) => console.log(err))
-			.pipe(fs.createWriteStream(`wallpaper-${wallpaperId}.png`))
-			.on('close', () => {
-				console.log('Download done');
-			});
+		if (src) {
+			downloadWallpaper(src);
+		} else {
+			console.error('WallPaper Random URL not found');
+		}
 	}
 });
