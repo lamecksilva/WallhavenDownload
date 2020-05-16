b34cebefd0a71d1b3a5c08e6da19e6c22972e11a
lamecksilva <lameck2@hotmail.com>
2020-05-16 13:40:37 -0300

    FIX: Remove env prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,6 @@ module.exports = {
 			name: 'WallHavenDownloadService',
 			script: 'index.js',
 			watch: true,
-			env: {
-				NODE_ENV: 'production',
-			},
 			ignore_watch: ['downloads', 'node_modules'],
 		},
 	],
